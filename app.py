d5b9d7b06841cc0d8eb6604e0af413cad84eecdc
Michal Nevo <nevosmic28@gmail.com>
2022-09-11 13:09:52 +0300

    connection is working

--- app.py
+++ app.py
@@ -2,22 +2,25 @@ from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-# '192.168.56.102'
-app.config['MYSQL_HOST'] = '192.168.56.102'
-app.config['MYSQL_PORT'] = 3306
+
+#app.config['MYSQL_HOST'] = '192.168.56.102'
+#app.config['MYSQL_PORT'] = 22
+
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'michalflask'
 app.config['MYSQL_PASSWORD'] = '28111992'
 app.config['MYSQL_DB'] = 'flaskdb'
 
-#mysql = MySQL(app)
+mysql = MySQL(app)
 
-"""
+
 @app.before_first_request
 def init_db():
     # Creating a connection cursor
     cursor = mysql.connection.cursor()
+    mysql.connection.ping(True) 
     # Executing SQL Statements
-    
+
     cursor.execute('''DROP TABLE IF EXISTS `flaskdb`.`Attendance`''')
     cursor.execute(''' CREATE TABLE `flaskdb`.`Attendance` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -26,8 +29,14 @@ def init_db():
   `total percentage` TEXT NOT NULL,
   `num of meetings` TEXT NOT NULL,
   PRIMARY KEY (`id`)); ''')
-"""
+    cursor.execute(''' INSERT INTO `flaskdb`.`Attendance`(`name`, `total time`, `total percentage`, `num of meetings`) VALUES (%s,%s,%s,%s)''', ("Michal", "650", "100.0", "2"))
+    cursor.execute(''' INSERT INTO `flaskdb`.`Attendance`(`name`, `total time`, `total percentage`, `num of meetings`) VALUES (%s,%s,%s,%s)''', ("Almog", "650", "100.0", "2"))
+    mysql.connection.commit()
+    #mysql.connection.close()  
 
+"""
+    
+"""
 @app.route('/')
 def index():
     return 'Hello world!!'
